c1ca27d2709e0e6751962e654163b806f9ed7494
AL-NFLY <almernfly@gmail.com>
2023-11-26 17:02:41 +0700

    feat: Add perks to main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,17 @@ const perks: PerksProps[] = [
   {
     name: 'Free sample',
     icon: FaHandHoldingDollar,
-    description: 'Each order above $250 will recieve a complementary sample',
+    description: 'Each order above $250 will recieve a complementary sample.',
   },
   {
     name: 'Free shipping',
     icon: FaTruck,
-    description: 'Enjoy seamless shopping with our lightning-fast shipping service',
+    description: 'Enjoy seamless shopping with our lightning-fast shipping service.',
   },
   {
     name: 'Secure Payment',
     icon: FaShield,
-    description: 'Experience worry-free transactions with our secure payment options'
+    description: 'Experience worry-free transactions with our secure payment options.',
   },
 ]
 
@@ -60,9 +60,29 @@ export default function Home() {
 
       <section className='border-t border-gray-200 bg-gray-50'>
         <MaxWidthWrapper className='py-20'>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 lg:gap-y-0 md:gap-x-6 lg:gap-x-8'>
-          
-          </div>  
+          <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 lg:gap-y-0 md:gap-x-6 lg:gap-x-8'>
+            {perks.map((perk) => (
+              <li
+                key={perk.name}
+                className='md:flex lg:block md:items-start text-center md:text-left lg:text-center'
+              >
+                <div className='flex justify-center md:flex-shrink-0'>
+                  <div className='h-16 w-16 flex items-center justify-center rounded-full bg-rose-100 text-rose-900'>
+                    {<perk.icon className='w-1/2 h-1/2' />}
+                  </div>
+                </div>
+
+                <div className='mt-6 md:mt-0 lg:mt-6 md:ml-4 lg:ml-0'>
+                  <h3 className='font-medium text-gray-900'>
+                    {perk.name}
+                  </h3>
+                  <p className='mt-2 text-sm text-muted-foreground'>
+                    {perk.description}
+                  </p>
+                </div>
+              </li>
+            ) )}
+          </ul>  
         </MaxWidthWrapper>
       </section>
     </>
