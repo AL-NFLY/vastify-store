ffe0c283e5418b1bb2e1442437ebe4eb16a84a77
AL-NFLY <almernfly@gmail.com>
2023-11-26 13:27:09 +0700

    feat: Add link & button in Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { Button, buttonVariants } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,11 +9,24 @@ export default function Home() {
       <div className='py-20 mx-auto flex flex-col text-center items-center max-w-3xl'>
         <h1 className='text-3xl md:text-4xl font-bold'>Vastify Store</h1>
         <p className='text-3xl md:text-4xl font-semibold tracking-tight text-slate-900'>Your one stop store solution for {' '}
-          <span className='bg-gradient-to-r from-amber-500 to-amber-400 bg-clip-text text-transparent'>fragrances.</span>
+          <span className='text-primary'>fragrances</span>.
         </p>
         <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
           Welcome to Vastify Store. Every asset on our platform is verified by our team to ensure our highest quality standards.
         </p>
+
+        <div className='flex flex-col md:flex-row mt-6 gap-4'>
+          <Link
+            href='/products'
+            className={buttonVariants()}
+          >
+          Browse Trending
+          </Link>
+
+          <Button variant='ghost'>
+            Our quality promise &rarr;
+          </Button>
+        </div>
       </div>
     </MaxWidthWrapper>
   )
