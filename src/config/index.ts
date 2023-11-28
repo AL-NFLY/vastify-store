f27279c5e04f4b6d5c82d24b34ce3f56f43a1dc5
AL-NFLY <almernfly@gmail.com>
2023-11-28 23:12:17 +0700

    feat: Create config/index.ts for navbar contents

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: 'COLLECTIONS',
+        value: 'collections' as const,
+        featured: [
+            {
+                name: 'Aqua',
+                href: '#',
+                imageSrc: '',
+            },
+            {
+                name: 'Oud',
+                href: '#',
+                imageSrc: '',
+            },
+            {
+                name: 'Baccarat Rouge 540',
+                href: '#',
+                imageSrc: '',
+            }
+        ] 
+    }
+]
