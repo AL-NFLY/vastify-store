5ba02c5bacb4ca63d0767c7f0cfe6982aaf3b757
AL-NFLY <almernfly@gmail.com>
2023-11-29 00:34:53 +0700

    feat: Add Button to NavItem.tsx

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { PRODUCT_CATEGORIES } from "@/config"
 import { Button } from "./ui/button"
+import { FaChevronDown } from "react-icons/fa6"
+import { cn } from "@/lib/utils"
 
-type Category = typeof PRODUCT_CATEGORIES[number]
+type Category = (typeof PRODUCT_CATEGORIES)[number]
 
 interface NavItemProps {
     category: Category
@@ -14,7 +18,19 @@ const NavItem = ({category, handleOpen, isAnyOpen, isOpen}: NavItemProps) => {
   return (
     <div className="flex">
         <div className="relative flex items-center">
-            <Button className="gap-1.5"></Button>
+            <Button 
+                className="gap-1.5"
+                onClick={handleOpen}
+                variant={isOpen ? 'secondary' : 'ghost'}
+            >
+                {category.label}
+                <FaChevronDown className={cn(
+                    'h-4 w-4 transition-all text-muted-foreground',
+                    {
+                        '-rotate-180': isOpen,
+                    }
+                )} />
+            </Button>
         </div>
     </div>
   )
