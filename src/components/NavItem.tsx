316d982f11467cf9c6f02fd701379c881c43ce1f
AL-NFLY <almernfly@gmail.com>
2023-11-29 00:22:51 +0700

    feat: Add types to NavItem.tsx

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,16 @@
+import { PRODUCT_CATEGORIES } from "@/config"
 import { Button } from "./ui/button"
 
-const NavItem = () => {
+type Category = typeof PRODUCT_CATEGORIES[number]
+
+interface NavItemProps {
+    category: Category
+    handleOpen: () => void
+    isOpen: boolean
+    isAnyOpen: boolean
+}
+
+const NavItem = ({category, handleOpen, isAnyOpen, isOpen}: NavItemProps) => {
   return (
     <div className="flex">
         <div className="relative flex items-center">
