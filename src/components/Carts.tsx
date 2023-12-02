58d0b55976af8d22373132e54f46b3f0d03c81de
AL-NFLY <almernfly@gmail.com>
2023-12-02 22:20:56 +0700

    feat: Add cart svg

--- src/components/Carts.tsx
+++ src/components/Carts.tsx
@@ -1,6 +1,20 @@
+'use client'
+
+import { FaCartShopping } from "react-icons/fa6"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+
 const Carts = () => {
   return (
-    <div>Carts</div>
+    <Sheet>
+        <SheetTrigger className="group flex justify-center">
+            <FaCartShopping
+                className=' h-4 w-auto flex-shrink-0 text-slate-900/75 group-hover:text-slate-900'
+            />
+            <span className="ml-1 text-sm font-medium text-slate-900/75 group-hover:text-slate-900">
+                0
+            </span>
+        </SheetTrigger>
+    </Sheet>
   )
 }
 
