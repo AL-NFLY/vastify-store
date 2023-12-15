52947b9aa03fd7c6aaf4369b387547846247f0da
AL-NFLY <almernfly@gmail.com>
2023-12-15 12:30:29 +0700

    feat: Add sign-in link

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from '@/components/Navbar'
+import { buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
@@ -9,6 +11,15 @@ const page = () => {
             <div className='flex flex-col items-center space-y-2 text-center'>
                 <h2 className='text-2xl font-semibold'>NEW CLIENT</h2>
                 <p className='text-slate-600'>Create a brand new account</p>
+
+                <Link
+                  href='/sign-in'
+                  className={buttonVariants({
+                    variant: 'link',
+                  })}
+                >
+                  Already have an account?
+                </Link>
             </div>
         </div>
     </div>
