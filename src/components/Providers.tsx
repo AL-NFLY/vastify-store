a6e7c5ebe503e4c04d6f488d61209216cfe025fe
AL-NFLY <almernfly@gmail.com>
2023-12-16 19:48:39 +0700

    feat: Implement trpc.Provider & tanstack to Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { useState } from "react"
+import { PropsWithChildren, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/trpc/client";
 import { httpBatchLink } from "@trpc/client";
 
-const Providers = () => {
+const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() => trpc.createClient({
     links: [
@@ -21,7 +21,14 @@ const Providers = () => {
     ]
   }))
   return (
-    <div>Providers</div>
+    <trpc.Provider
+        client={trpcClient}
+        queryClient={queryClient}
+    >
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    </trpc.Provider>
   )
 }
 
