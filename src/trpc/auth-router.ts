eaf7a1120435eac98f8baefe2c4029586abdb585
AL-NFLY <almernfly@gmail.com>
2023-12-18 23:17:31 +0700

    feat: Add required data to createPayloadUser & its return

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -26,8 +26,15 @@ export const authRouter = router({
             await payload.create({
                 collection: 'users',
                 data: {
-                    
-                }
+                    email,
+                    password,
+                    role: 'user',
+                },
             })
+
+            return { 
+                success: true, 
+                sentToEmail: email,
+            }
     })
 })
