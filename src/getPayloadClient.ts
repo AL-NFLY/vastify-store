51a4a548d805beae95421e04bce5357d07965c22
AL-NFLY <almernfly@gmail.com>
2023-12-18 19:23:25 +0700

    bugfix: Add return cache.client

--- src/getPayloadClient.ts
+++ src/getPayloadClient.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path"
-import payload from "payload";
+import payload, { Payload } from "payload";
 import { InitOptions } from "payload/config";
 
 dotenv.config({
@@ -45,6 +45,7 @@ const getPayloadClient = async ({
     cached.promise = null
     throw error
   }
+  return cached.client
 }
 
 export default getPayloadClient
