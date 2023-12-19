1e71004938794b67297469e67883a4e894501e72
AL-NFLY <almernfly@gmail.com>
2023-12-19 13:10:01 +0700

    feat: Add types for verify-email

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,4 +1,10 @@
-const VerifyEmailPage = () => {
+interface PageProps {
+    searchParams: {
+        [key: string] : string | string[] | undefined
+    }
+}
+
+const VerifyEmailPage = ({ searchParams }: PageProps) => {
     return (
         <div className="container flex flex-col items-center justify-center h-[calc(100vh-72px)] lg:px-0">
             <div className="flex flex-col mx-auto w-full sm:w-80 justify-center">
