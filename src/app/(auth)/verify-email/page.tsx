bd3efe809ffb762a8d9b193fb5491d35e7362d9e
AL-NFLY <almernfly@gmail.com>
2023-12-19 13:20:45 +0700

    feat: Add if condition for email verification

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -5,10 +5,21 @@ interface PageProps {
 }
 
 const VerifyEmailPage = ({ searchParams }: PageProps) => {
+    const token = searchParams.token
+    const toEmail = searchParams.to
+
     return (
         <div className="container flex flex-col items-center justify-center h-[calc(100vh-72px)] lg:px-0">
             <div className="flex flex-col mx-auto w-full sm:w-80 justify-center">
-                
+                {token && typeof token === 'string' ? (
+                    <div className="grid gap-6">
+
+                    </div>
+                ) : (
+                    <div className="flex flex-col h-full items-center justify-center space-y-2">
+                        <h2 className="text-2xl font-semibold">CHECK YOUR EMAIL</h2>
+                    </div>
+                )}
             </div>
         </div>
     )
