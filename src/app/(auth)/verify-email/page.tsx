35195b139c1478b37109ead53b1a73f9b204d221
AL-NFLY <almernfly@gmail.com>
2023-12-19 13:06:39 +0700

    feat: Create page.tsx for verify-email

---
+++ src/app/(auth)/verify-email/page.tsx
@@ -0,0 +1,11 @@
+const VerifyEmailPage = () => {
+    return (
+        <div className="container flex flex-col items-center justify-center h-[calc(100vh-72px)] lg:px-0">
+            <div className="flex flex-col mx-auto w-full sm:w-80 justify-center">
+                
+            </div>
+        </div>
+    )
+}
+
+export default VerifyEmailPage
