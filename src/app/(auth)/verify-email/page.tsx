849319d81f9abb044fef2d95e2c44e4f8acc90f8
AL-NFLY <almernfly@gmail.com>
2023-12-19 13:35:01 +0700

    feat: Add if condition for email name

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -10,14 +10,25 @@ const VerifyEmailPage = ({ searchParams }: PageProps) => {
 
     return (
         <div className="container flex flex-col items-center justify-center h-[calc(100vh-72px)] lg:px-0">
-            <div className="flex flex-col mx-auto w-full sm:w-80 justify-center">
+            <div className="flex flex-col mx-auto w-full justify-center">
                 {token && typeof token === 'string' ? (
                     <div className="grid gap-6">
 
                     </div>
                 ) : (
-                    <div className="flex flex-col h-full items-center justify-center space-y-2">
+                    <div className="flex flex-col h-full items-center text-center space-y-2">
                         <h2 className="text-2xl font-semibold">CHECK YOUR EMAIL</h2>
+                        {toEmail ? (
+                            <p className="text-slate-600">
+                                We have sent a verification link to{' '}
+                                <span className="text-primary">{toEmail}</span>
+                                .
+                            </p>
+                            ) : (
+                            <p className="text-slate-600">
+                                We have sent a verification link to your email.
+                            </p>
+                        )}
                     </div>
                 )}
             </div>
