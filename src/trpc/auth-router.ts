cbdc817abbf42e52e6f176cd5ea5f070e2a5a38b
AL-NFLY <almernfly@gmail.com>
2023-12-20 12:23:08 +0700

    feat: Add email verification method

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from "../lib/validators/AuthCredentialsValid
 import getPayloadClient from "../getPayloadClient";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const authRouter = router({
     createPayloadUser: publicProcedure
@@ -36,5 +37,20 @@ export const authRouter = router({
                 success: true, 
                 sentToEmail: email,
             }
-    })
+    }),
+
+    verifyEmail: publicProcedure
+        .input(z.object({
+            token: z.string()
+        }))
+        .mutation(async ({ input }) => {
+            const { token } = input
+            
+            const payload = await getPayloadClient()
+
+            const isVerified = await payload.verifyEmail({
+                collection: 'users',
+                token,
+            })
+        })
 })
