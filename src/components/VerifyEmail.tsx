2e01a66341e467b1a19ec250c55c2220423e62a3
AL-NFLY <almernfly@gmail.com>
2023-12-20 13:58:41 +0700

    feat: Add if condition for successful verification

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { trpc } from "@/trpc/client"
-import { FaCircleXmark } from "react-icons/fa6"
+import { FaCircleCheck, FaCircleXmark } from "react-icons/fa6"
 
 interface VerifyEmailProps {
   token: string,
@@ -22,6 +22,16 @@ const VerifyEmail = ({ token }: VerifyEmailProps) => {
         <FaCircleXmark className="h-9 w-9 text-red-600"/>
         <h2 className="mt-2 text-2xl font-semibold">WE ENCOUNTERED AN ERROR</h2>
         <p className="text-sm">This token has expired or is not valid!</p>
+      </div>
+    )
+  }
+
+  if (data?.success) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-y-2">
+        <FaCircleCheck className="h-9 w-9 text-green-600" />
+        <h2 className="mt-2 text-2xl font-semibold">EMAIL VERIFIED</h2>
+        <p className="text-slate-600">Thank you for verifying your email.</p>
       </div>
     )
   }
