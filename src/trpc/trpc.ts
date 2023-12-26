d737377ebd9861bb0fe13daad7371143dc50f065
AL-NFLY <almernfly@gmail.com>
2023-12-26 13:02:52 +0700

    feat: Import ExpressContextProps to trpc.ts as generic

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,7 @@
+import { ExpressContextProps } from "@/server";
 import { initTRPC } from "@trpc/server";
 
-const initiateTrpc = initTRPC.context().create()
+const initiateTrpc = initTRPC.context<ExpressContextProps>().create()
 
 export const router = initiateTrpc.router
 export const publicProcedure = initiateTrpc.procedure
