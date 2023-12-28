50a8541c2a230b447e296880aa2af65b4d968a46
AL-NFLY <almernfly@gmail.com>
2023-12-28 16:40:48 +0700

    feat: Implement dropdown-menu for Nav

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "./ui/button"
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu"
 
 const UserNav = () => {
   return (
@@ -10,8 +10,26 @@ const UserNav = () => {
             className="overflow-visible"
             asChild
         >
-            <Button>My Account</Button>
+            <Button
+                className="relative"
+                variant="ghost"
+            >
+                My Account
+            </Button>
         </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+            className="bg-white w-60"
+            align="end"
+        >
+            <div className="flex items-center justify-start gap-2 p-2">
+                <div className="flex flex-col space-y-0.5 leading-none">
+                    <p className="font-medium text-sm text-black">
+                        Username
+                    </p>
+                </div>
+            </div>
+        </DropdownMenuContent>
     </DropdownMenu>
   )
 }
