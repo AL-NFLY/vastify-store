8c10f45c004158c9ea72ff13bf55003907c5f837
AL-NFLY <almernfly@gmail.com>
2023-12-29 12:24:04 +0700

    feat: Add product price & description in collection

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -9,6 +9,7 @@ export const Products: CollectionConfig = {
 
     },
     fields: [
+        // User relation (hidden)
         {
             name: 'user',
             type: 'relationship',
@@ -23,6 +24,20 @@ export const Products: CollectionConfig = {
             name: 'name',
             label: 'Name',
             type: 'text',
+            required: true,
+        },
+        {
+            name: 'description',
+            label: 'Product Description',
+            type: 'textarea',
+            required: true,
+        },
+        {
+            name: 'price',
+            label: 'Price in USD',
+            type: 'number',
+            min: 0,
+            max: 1000,
             required: true,
         },
     ],
