692e209200458326c64a3f9fd31a41f5c3f09532
AL-NFLY <almernfly@gmail.com>
2024-01-01 11:52:15 +0700

    feat: Add purchasedProductFileIds & return spread of owned & purchased file id

--- src/collections/ProductFiles.ts
+++ src/collections/ProductFiles.ts
@@ -34,9 +34,39 @@ const ownedOrPurchased: Access = async ({ req }) => {
     const ownedProductFileIds = products.map((prod) => prod.product_files).flat()
     const { docs: orders } = await req.payload.find({
         collection: 'orders',
-        depth: 0,
-        where:
+        depth: 2,
+        where: {
+            user: {
+                equals: user.id,
+            },
+        },
     })
+
+    const purchasedProductFileIds = orders.map((order) => {
+        // Get product(s) in Order
+        return order.products.map((product) => {
+            if (typeof product === 'string') {
+                return req.payload.logger.error(
+                    'Search depth not sufficient to find purchased file IDs'
+                )
+
+            }
+            return typeof product.product_files === 'string' 
+                ? product.product_files 
+                : product.product_files.id
+        })
+    })
+    .filter(Boolean)
+    .flat()
+
+    return {
+        id: {
+            in: [
+                ...ownedProductFileIds,
+                ...purchasedProductFileIds,
+            ]
+        }
+    }
 }
 
 export const ProductFiles: CollectionConfig = {
