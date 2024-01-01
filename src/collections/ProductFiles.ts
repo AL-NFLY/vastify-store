fb959f43976dcc04e00309cf12207913c44a2803
AL-NFLY <almernfly@gmail.com>
2024-01-01 11:37:48 +0700

    feat: Generate types & implement Orders Collection

--- src/collections/ProductFiles.ts
+++ src/collections/ProductFiles.ts
@@ -1,6 +1,6 @@
 import { User } from "../payload-types";
 import { BeforeChangeHook } from "payload/dist/collections/config/types";
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
 
 const addUser: BeforeChangeHook = ({
     req, data
@@ -13,6 +13,32 @@ const addUser: BeforeChangeHook = ({
     }
 }
 
+const ownedOrPurchased: Access = async ({ req }) => {
+    const user = req.user as User | null
+
+    if (user?.role === 'admin')
+        return true
+    if (!user)
+        return false
+
+    const { docs: products } = await req.payload.find({
+        collection: 'products',
+        depth: 0,
+        where: {
+            user: {
+                equals: user.id,
+            },
+        },
+    })
+
+    const ownedProductFileIds = products.map((prod) => prod.product_files).flat()
+    const { docs: orders } = await req.payload.find({
+        collection: 'orders',
+        depth: 0,
+        where:
+    })
+}
+
 export const ProductFiles: CollectionConfig = {
     slug: 'product_files',
     admin: {
@@ -20,6 +46,9 @@ export const ProductFiles: CollectionConfig = {
     },
     hooks: {
         beforeChange: [addUser]
+    },
+    access: {
+
     },
     upload: {
         staticURL: '/product_files',
