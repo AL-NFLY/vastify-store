0ba4068b95b2fc8aeda23e029a270081d016a889
AL-NFLY <almernfly@gmail.com>
2024-01-02 23:23:09 +0700

    feat: Add layout for ProductReel

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ProductReel = () => {
   return (
-    <div>ProductReel</div>
+    <section className='py-12'>
+        <div className='md:flex md:items-center md:justify-between mb-4'>
+            <div className='max-w-2xl lg:max-w-4xl px-4 lg:px-0'>
+                Title
+            </div>
+        </div>
+    </section>
   )
 }
 
