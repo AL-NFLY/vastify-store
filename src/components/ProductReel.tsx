1fcbecbbf4ef9732cf5b37e13845e96935d033fa
AL-NFLY <almernfly@gmail.com>
2024-01-02 23:31:29 +0700

    feat: Implement optional subtitle to ProductReel

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -14,9 +14,14 @@ const ProductReel = (props: ProductReelProps) => {
         <div className='md:flex md:items-center md:justify-between mb-4'>
             <div className='max-w-2xl lg:max-w-4xl px-4 lg:px-0'>
                 {title ? (
-                    <h1 className='text-2xl md:text-3xl font-bold text-zinc-900'>
+                    <h1 className='text-2xl md:text-3xl font-bold text-slate-900'>
                         {title}
                     </h1>
+                ) : null}
+                {subtitle ? (
+                    <p className='mt-2 text-sm text-slate-600'>
+                        {subtitle}
+                    </p>
                 ) : null}
             </div>
         </div>
