448cb4cf6188bf05a5080f72a8cd14e17c01f7ce
AL-NFLY <almernfly@gmail.com>
2024-01-02 23:16:14 +0700

    feat: Add update access only for admin

--- src/collections/ProductFiles.ts
+++ src/collections/ProductFiles.ts
@@ -79,6 +79,7 @@ export const ProductFiles: CollectionConfig = {
     },
     access: {
         read: ownedOrPurchased,
+        update: ({ req }) => req.user.role === 'admin',
     },
     upload: {
         staticURL: '/product_files',
