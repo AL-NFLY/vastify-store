8ba1b56c485a849b5e6fb38d37c0ef9d250a2ccb
AL-NFLY <almernfly@gmail.com>
2024-01-02 23:29:51 +0700

    feat: Implement title to ProductReel

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -7,11 +7,17 @@ interface ProductReelProps {
 }
 
 const ProductReel = (props: ProductReelProps) => {
+  const { title, subtitle, href } = props
+  
   return (
     <section className='py-12'>
         <div className='md:flex md:items-center md:justify-between mb-4'>
             <div className='max-w-2xl lg:max-w-4xl px-4 lg:px-0'>
-                Title
+                {title ? (
+                    <h1 className='text-2xl md:text-3xl font-bold text-zinc-900'>
+                        {title}
+                    </h1>
+                ) : null}
             </div>
         </div>
     </section>
